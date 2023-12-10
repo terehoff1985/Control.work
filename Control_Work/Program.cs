bab05b68b8458579a79ab5694645a4621c9fa89b
Terehoff <Artem>
2023-12-10 10:14:12 +0700

    ввод массива с клавиатуры

---
+++ Control_Work/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Введите строки массива через запятую:");
+        string[] inputStrings = Console.ReadLine().Split(',');
+
+       
+        string[] resultArray = FilterStrings(inputStrings);
+
+        
+        Console.WriteLine("Результирующий массив:");
+        foreach (string str in resultArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+     
+ 
+}
